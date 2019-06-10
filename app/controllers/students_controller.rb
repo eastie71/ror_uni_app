fb8841e746e4015e28d2995e2dd71cf6b623cbe9
Craig Eastwood <craigdavideastwood@gmail.com>
2019-06-10 12:32:04 +1000

    Added initial student index page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,7 +4,14 @@ class StudentsController < ApplicationController
 
 	def create
 	end
-	
+
 	def index
+		@students = Student.all
+	end
+
+	def update
+	end
+
+	def show
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 	root 'courses#index'
 	get 'courses/new', to: 'courses#new'
+	resources :students, except: [:destroy]
 end
